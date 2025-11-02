46b815d47692207602d236b79a4b309c52a84002
seobeomju <alt10063@naver.com>
2025-11-02 20:43:26 +0900

    test(domain): Lotto 클래스 단위 테스트 보강
    - LottoTest를 domain 패키지로 이동
    - 기존 테스트 (6개, 중복)에 예외 메시지 검증 추가
    - 1~45 범위 검증 테스트 추가 (@ParameterizedTest)

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,9 +1,14 @@
 package lotto;
 
+import java.util.stream.Stream;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,5 +26,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidRangeNumbers") // 아래 static 메서드에서 테스트 케이스를 가져옴
+    void createLotto_InvalidRange(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    // createLotto_InvalidRange 테스트를 위한 데이터
+    private static Stream<Arguments> invalidRangeNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 0)),  // 0 포함
+                Arguments.of(List.of(1, 2, 3, 4, 5, 46)), // 46 포함
+                Arguments.of(List.of(1, 2, 3, 4, 5, -1))  // 음수 포함
+        );
+    }
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
